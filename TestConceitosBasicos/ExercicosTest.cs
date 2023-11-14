c5bab15c5fbe013a8dbe690afb5057e7c93b6539
rosenclever <rosenclever@gmail.com>
2023-11-14 15:03:55 -0300

    Classe ExerciocsTest criada, com método de teste TestaPrimoComValorPrimo

---
+++ TestConceitosBasicos/ExercicosTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConceitosBasicos
+{
+    [TestClass]
+    public class ExercicosTest
+    {
+        [TestMethod]
+        public void TestaPrimoComValorPrimo()
+        {
+            //cenario
+            int primo = 13;
+
+            //acao e verificacao
+            Assert.IsTrue(VerificarPrimo.Validar(primo));
+        }
+    }
+}
