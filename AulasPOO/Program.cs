a55e229b1f2799cfc08e6993f724411ccb796c3c
rosenclever <rosenclever@gmail.com>
2023-11-14 16:49:40 -0300

    Teste da classe cliente com descricao da classe no arquivo Program.cs do Projeto AulasPOO

---
+++ AulasPOO/Program.cs
@@ -0,0 +1,5 @@
+﻿/*
+ * O projeto deverá conter uma classe cliente que possui nome, endereco, telefone, CPF e data de nascimento
+ * A classe cliente será criada dentro de uma pasta chamada Model
+ * Deverá ser possível verificar a idade do cliente a partir de um método da prória classe
+ */
---
+++ TestAulasPOO/UnitTest1.cs
@@ -0,0 +1,18 @@
+namespace TestAulasPOO
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestaConstrutorDefaultCliente()
+        {
+            //este código verifica a criação de instancias da classe cliente pelo contrutor default
+        }
+
+        [TestMethod]
+        public void TestaConstrutorClienteComNome() 
+        {
+            //este código verifica a criação de instancias da classe cliente informando o seu nome
+        }
+    }
+}
